6d14a45e58571ca11ebe508feb3cf92ea3a767fe
alexgitrepo <alex.dev4web.com>
2020-03-03 17:57:25 +0500

    Add simple start in package.json

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use('/todo-lists', todolists)
-app.listen(7542, () => {
+app.listen(process.env.PORT, () => {
     console.log("server starts at port 7542")
 })
