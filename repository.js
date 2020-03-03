f5935d200aa608e20f0a7bc027d926810782888f
alexgitrepo <alex.dev4web.com>
2020-03-03 18:07:14 +0500

    All settings done

--- repository.js
+++ repository.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/todoListsDB', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 const todoListSchema = new mongoose.Schema({
     title: {type: String, required: true},
     created_at: {type: Date, required: true, default: Date.now}
